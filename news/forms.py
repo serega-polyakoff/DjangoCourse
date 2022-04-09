4e38e8301275aeb425e044b3b507cc2db625319c
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-09 22:40:46 +0300

    django_L_9_1

---
+++ news/forms.py
@@ -0,0 +1,8 @@
+from .models import Articles
+from django.forms import ModelForm
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'full_text', 'date']
