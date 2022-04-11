03175e385d9a142e313269ca2cfc2d9094cb3117
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-11 17:59:08 +0300

    django_L_9_4

--- news/views.py
+++ news/views.py
@@ -3,17 +3,17 @@ from .models import Articles
 from .forms import ArticlesForm
 
 def news_home(request):
-    news = Articles.objects.all()
-    return render(request, 'news/news_home.html', {'news': news })
+    news = Articles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
 
 
 def create(request):
     error = ''
-    if request.methot == 'POST':
+    if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('news_home')
         else:
             error = 'Форма была неверной'
 
