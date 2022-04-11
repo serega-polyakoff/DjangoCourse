e2f2d380b3c5ebede5f41a613a4f3cb7978c56dc
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-11 17:35:30 +0300

    django_L_9_3

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,27 @@
 from .models import Articles
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
         fields = ['title', 'anons', 'full_text', 'date']
+
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название статьи'
+            }),
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "title": DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Дата публикации'
+            }),
+            "title": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            })
+        }
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
 
@@ -8,9 +8,20 @@ def news_home(request):
 
 
 def create(request):
+    error = ''
+    if request.methot == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
     form = ArticlesForm()
 
     data = {
-        'form': form
+        'form': form,
+        'error': error
     }
+
     return render(request, 'news/create.html')
