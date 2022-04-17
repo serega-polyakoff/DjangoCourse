9f5d04b70d18969821037bd332849c87c90a3709
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-17 20:14:56 +0300

    django_L_10_1

--- news/views.py
+++ news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def create(request):
@@ -15,7 +21,7 @@ def create(request):
             form.save()
             return redirect('news_home')
         else:
-            error = 'Форма была неверной'
+            error = ''
 
     form = ArticlesForm()
 
