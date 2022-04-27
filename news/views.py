a0c17b0ce82ae53f6b862a70e3564f75dff50f90
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-27 16:16:24 +0300

    django_L_11_1

--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
+
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
@@ -11,6 +12,11 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
 
 
 def create(request):
