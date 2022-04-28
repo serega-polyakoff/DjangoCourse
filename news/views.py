1219516b149e3966d0ab14732251df4d1704e8f0
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-04-28 20:40:34 +0300

    django_L_11_2

--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, DeleteView
+
 
 
 def news_home(request):
@@ -17,6 +18,11 @@ class NewsUpdateView(UpdateView):
     model = Articles
     template_name = 'news/create.html'
     form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
 
 
 def create(request):
